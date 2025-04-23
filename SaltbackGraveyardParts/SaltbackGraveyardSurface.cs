78024f90b1f7839cfff5cdbd67ed68421e099eca
Patatifique <patatifique@icloud.com>
2025-04-23 09:54:53 +0200

    Added Saltback Graveyard secret

---
+++ SaltbackGraveyardParts/SaltbackGraveyardSurface.cs
@@ -0,0 +1,39 @@
+using Qud.API;
+using System;
+
+#nullable disable
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class SaltbackGraveyardSurface : IPart
+    {
+        public string RegionName = "Saltback Graveyard";
+        public string RevealString;
+        public string RevealSecret = "$saltbackgraveyard";
+        public string RevealKey = "SaltbackGraveryard_LocationKnown";
+
+        public override bool SameAs(IPart p) => false;
+
+        public override void Register(GameObject Object, IEventRegistrar Registrar)
+        {
+            Registrar.Register("EnteredCell");
+            base.Register(Object, Registrar);
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "EnteredCell" && !The.Game.HasIntGameState(this.RevealKey))
+            {
+                The.Game.SetIntGameState(this.RevealKey, 1);
+                ZoneManager.instance.GetZone("JoppaWorld").BroadcastEvent("SaltbackGraveyardReveal");
+                JournalMapNote mapNote = JournalAPI.GetMapNote(this.RevealSecret);
+                if (mapNote != null && !mapNote.Revealed)
+                {
+                    JournalAPI.AddAccomplishment("You discovered the hidden Saltback Graveyard.", "<spice.commonPhrases.intrepid.!random.capitalize> =name= discovered the Saltback Graveyard, once thought lost to the sands of time.", $"<spice.commonPhrases.allThroughout.!random.capitalize> =year=, =name= followed tortoises. {The.Player.GetPronounProvider().CapitalizedSubjective} became known as the saviour of Saltbacks.", muralCategory: MuralCategory.VisitsLocation);
+                    JournalAPI.RevealMapNote(mapNote);
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
---
+++ SaltbackGraveyardParts/SaltbackGraveyardTerrain.cs
@@ -0,0 +1,45 @@
+using Qud.API;
+using System;
+
+#nullable disable
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class SaltbackGraveyardTerrain : IPart
+    {
+        public string secretId = "$saltbackgraveyard";
+        public bool revealed;
+
+        public override bool SameAs(IPart p) => false;
+
+        public override void Register(GameObject Object, IEventRegistrar Registrar)
+        {
+            Object.SetIntProperty("ForceMutableSave", 1);
+            Registrar.Register("SaltbackGraveyardReveal");
+            base.Register(Object, Registrar);
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "SaltbackGraveyardReveal")
+            {
+                this.ParentObject.Render.Tile = "Terrain/map_graveyard.png";
+                this.ParentObject.Render.ColorString = "&W";
+                this.ParentObject.Render.DisplayName = "Saltback Graveyard";
+                this.ParentObject.Render.DetailColor = "r";
+                this.ParentObject.Render.RenderString = "#";
+                this.ParentObject.HasProperName = true;
+                this.ParentObject.GetPart<Description>().Short = "Some Description";
+                this.ParentObject.GetPart<TerrainTravel>()?.ClearEncounters();
+                this.ParentObject.SetStringProperty("OverlayColor", "&W");
+                if (this.secretId != null)
+                {
+                    JournalMapNote mapNote = JournalAPI.GetMapNote(this.secretId);
+                    if (mapNote != null && !mapNote.Revealed)
+                        mapNote.Reveal((string)null, false);
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
