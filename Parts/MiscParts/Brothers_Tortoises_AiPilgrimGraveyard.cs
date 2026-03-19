cde5fc119b02419d4c9e778f1ae697ccd3d7b224
Balthichou <153240971+Balthichou@users.noreply.github.com>
2026-03-19 17:29:21 +0100

    Issachari encampment + general names cleanup

--- Parts/MiscParts/Brothers_Tortoises_AiPilgrimGraveyard.cs
+++ Parts/MiscParts/Brothers_Tortoises_AiPilgrimGraveyard.cs
@@ -21,7 +21,7 @@ namespace XRL.World.Parts
         public int GraveyardZx = 10;
         public string GraveyardZoneID = The.Game.GetStringGameState("SaltbackGraveyardZoneID");
         public string GraveyardEntranceZoneID = The.Game.GetStringGameState("SaltbackGraveyardZoneID");
-        public string TargetObject = "Brothers_Tortoises_Big Stele";
+        public string TargetObject = "Brothers_Tortoises_BigStele";
         public string MapNoteAttributes;
         public int Chance = 100;
         public bool Ignore;
